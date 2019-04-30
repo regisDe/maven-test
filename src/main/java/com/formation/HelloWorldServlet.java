c5b8ed72429cd8133ff059aa5401cdb9b23ff143
regisDe <regisDe@users.noreply.github.com>
2019-04-30 09:12:10 +0200

    Update HelloWorldServlet.java

--- src/main/java/com/formation/HelloWorldServlet.java
+++ src/main/java/com/formation/HelloWorldServlet.java
@@ -4,8 +4,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
 
-public class HelloWorldServlet extends HttpServlet 
-{
+public class HelloWorldServlet extends HttpServlet {
   public void service( HttpServletRequest req, HttpServletResponse res ) throws IOException {
     PrintWriter out = res.getWriter();
     out.println( "Hello, World!" );
